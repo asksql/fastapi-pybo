63472c6e94458edb90f736d142584924c139df2b
jhpark <jongheepark@sbs.co.kr>
2024-12-19 17:50:51 +0900

    파이보 프로젝트 최초 커밋

---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { mount } from "svelte";
+import App from "./App.svelte";
+
+const app = mount(App, {
+    target: document.getElementById("app"),
+});
+
+export default app;
